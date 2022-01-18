0162a58ec0bac2b135a5f592937959e91b2d212b
Jakub Fábik <jfabik@student.umb.sk>
2022-01-18 10:35:58 +0200

    created main sceleton

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
-import { StyleSheet, Text, TextInput, View, } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import react, {useState} from 'react';
 import DropDownPicker from 'react-native-dropdown-picker';
+import RadioForm from 'react-native-simple-radio-button';
 
 export default function App() {
   const [weight,setWeight] = useState(0);
   const [bottle,setBottle] = useState(0);
+  const [hour,setHour] = useState(0);
+  const [gender, setGender] = useState('male');
   //for ios DropDownPicker:
-  const [open, setOpen] = useState(false);
+  const [openBT, setOpenforBottles] = useState(false);
+  const [openHR, setOpenforHours] = useState(false);
 
-  const bottles = Array();
+  const counter_list = Array();
   for(let i=0;i<20;i++){
-    bottles.push({label:i.toString(),value:i})
+    counter_list.push({label:i.toString(),value:i})
+  }
+
+  const genders = [{label: 'Male',value: 'male'},
+  {label: 'Female',value: 'female'},
+ ]
+
+  function calculate(){
+    return null;
   }
 
   return (
@@ -24,22 +36,39 @@ export default function App() {
       />
       <Text>Bottles</Text>
       {/* <DropDownPicker style={styles.label} selectedValue={bottle} onValueChange={(itemValue) => setBottle(itemValue)}>
-        {bottles.map((bottles) => ( 
-        <DropDownPicker.Item label={bottles.label + (bottles.value == 1 ? ' bottle':' bottles')} value={bottles.value} />
+        {counter_list.map((counter_list) => ( 
+        <DropDownPicker.Item label={counter_list.label + (counter_list.value == 1 ? ' bottle':' counter_list')} value={counter_list.value} />
       ))}
       </DropDownPicker> */}
       <DropDownPicker 
-        open={open}
-        //value={bottles.label + (bottles.value == 1 ? ' bottle':' bottles')} value={bottles.value}
-        items={bottles}
+        open={openBT}
+        //value={counter_list.label + (counter_list.value == 1 ? ' bottle':' counter_list')} value={counter_list.value}
+        items={counter_list}
         placeholder={bottle?("Selected "  + bottle.toString() + (bottle == 1 ? ' bottle ':' bottles ')):("No bottle selected")}
-        setOpen={setOpen}
+        setOpen={setOpenforBottles}
         setValue={setBottle}
         autoScroll={true}
       />
       {console.log("selected bottles: " + bottle)}
       <Text>Time</Text>
+      <DropDownPicker 
+        open={openHR}
+        items={counter_list}
+        placeholder={hour?("Selected "  + hour.toString() + (hour == 1 ? ' hour ':' houres ')):("No selected hours")}
+        setOpen={setOpenforHours}
+        setValue={setHour}
+        autoScroll={true}
+      />
+      {console.log("selected hours: " + hour)}
       <Text>Gender</Text>
+      <RadioForm
+      initial={0}
+      onPress={(value) => {setGender(value)}}
+      radio_props={genders}
+      style={styles.label}
+      >
+      </RadioForm>
+      <Button onPress={calculate} style={styles.label} title="Calculate"></Button>
     </View>
   );
 }
